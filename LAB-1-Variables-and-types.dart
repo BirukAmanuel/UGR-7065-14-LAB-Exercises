1fc10cfb72cd3777b16d66a8b2ccf12e8239e42a
Biruk Amanuel <buraaman20@gmail.com>
2024-03-08 15:28:24 +0300

    variables and types exercise 1 done

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,8 @@
+void main() {
+  String myName = "Biruk Amnauel";
+  int myAge = 21;
+  String myFavoriteColor = "pink";
+  print("I am $myName.");
+  print("$myAge years of age.");
+  print("$myFavoriteColor is the color that i like.");
+}
