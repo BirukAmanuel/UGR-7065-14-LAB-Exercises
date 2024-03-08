760a009c89932ee6529a3c63fc31e5f95c6ee719
Biruk Amanuel <buraaman20@gmail.com>
2024-03-08 16:46:07 +0300

    Functions exercise 2

---
+++ LAB-2-Functions.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+void main() {
+  print("Enter the lower bound of the range:");
+  int lowerBound = int.parse(stdin.readLineSync()!);
+  print("Enter the upper bound of the range:");
+  int upperBound = int.parse(stdin.readLineSync()!);
+  print("Prime numbers within the range $lowerBound to $upperBound:");
+  for (int i = lowerBound; i <= upperBound; i++) {
+    if (isPrime(i)) {
+      print(i);
+    }
+  }
+}
+
+bool isPrime(int number) {
+  if (number <= 1) {
+    return false;
+  }
+  for (int i = 2; i <= number / 2; i++) {
+    if (number % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
